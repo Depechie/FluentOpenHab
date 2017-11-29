c0b02eddcc2a882ff722684cc50cf39741bf21e9
Depechie <glenn.versweyveld@gmail.com>
2017-11-29 23:08:33 +0100

    VariableSizedWrapGrid

--- FluentOpenHab/MainPage.xaml.cs
+++ FluentOpenHab/MainPage.xaml.cs
@@ -6,20 +6,22 @@ namespace FluentOpenHab
     public class OpenHabThing
     {
         public string Title { get; set; }
-        public int Width { get; set; } = 1;
-        public int Height { get; set; } = 1;
+        public int ColSpan { get; set; } = 1;
+        public int RowSpan { get; set; } = 1;
 
-        public double CalculateHeight(int height) => height * Height;
-        public double CalculateWidth(int width) => width * Width;
-
+        public double CalculateHeight(int height) => height * RowSpan;
+        public double CalculateWidth(int width) => width * ColSpan;
     }
 
     public sealed partial class MainPage : Page
     {
         public List<OpenHabThing> Items { get; set; } = new List<OpenHabThing>()
         {
-            new OpenHabThing() { Title = "First floor", Height = 2 },
-            new OpenHabThing() { Title = "Second floor", Width = 2 }
+            new OpenHabThing() { Title = "First floor", RowSpan = 2 },
+            new OpenHabThing() { Title = "Second floor", ColSpan = 2 },
+            new OpenHabThing() { Title = "Second floor" },
+            new OpenHabThing() { Title = "First floor", RowSpan = 2 },
+            new OpenHabThing() { Title = "Second floor", ColSpan = 2 }
         };
 
         public MainPage()

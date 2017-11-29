dc2221e53f89915bddc4e92d2077c721779405f1
Depechie <glenn.versweyveld@gmail.com>
2017-11-30 00:32:08 +0100

    Grouped

--- FluentOpenHab/MainPage.xaml.cs
+++ FluentOpenHab/MainPage.xaml.cs
@@ -3,6 +3,12 @@ using Windows.UI.Xaml.Controls;
 
 namespace FluentOpenHab
 {
+    public class OpenHabGroup
+    {
+        public string Name { get; set; }
+        public List<OpenHabThing> Items { get; set; }
+    }
+
     public class OpenHabThing
     {
         public string Title { get; set; }
@@ -15,19 +21,40 @@ namespace FluentOpenHab
 
     public sealed partial class MainPage : Page
     {
-        public List<OpenHabThing> Items { get; set; } = new List<OpenHabThing>()
-        {
-            new OpenHabThing() { Title = "Item 1", ColSpan = 2, RowSpan = 2 },
-            new OpenHabThing() { Title = "Item 2", ColSpan = 2 },
-            new OpenHabThing() { Title = "Item 3" },
-            new OpenHabThing() { Title = "Item 4", ColSpan = 2, RowSpan = 2 },
-            new OpenHabThing() { Title = "Item 5" },
-            new OpenHabThing() { Title = "Item 6", ColSpan = 2 }
-        };
+        public List<OpenHabGroup> Groups { get; set; } = new List<OpenHabGroup>();
 
         public MainPage()
         {
             this.InitializeComponent();
+
+            Groups.Add(new OpenHabGroup()
+            {
+                Name = "Group 1",
+                Items = new List<OpenHabThing>()
+                {
+                    new OpenHabThing() { Title = "Item 1", ColSpan = 2, RowSpan = 2 },
+                    new OpenHabThing() { Title = "Item 2", ColSpan = 2 },
+                    new OpenHabThing() { Title = "Item 3" },
+                    new OpenHabThing() { Title = "Item 4", ColSpan = 2, RowSpan = 2 },
+                    new OpenHabThing() { Title = "Item 5" },
+                    new OpenHabThing() { Title = "Item 6", ColSpan = 2 }
+                }
+            });
+
+            Groups.Add(new OpenHabGroup()
+            {
+                Name = "Group 2",
+                Items = new List<OpenHabThing>()
+                {
+                    new OpenHabThing() { Title = "Item 1", ColSpan = 2, RowSpan = 2 },
+                    new OpenHabThing() { Title = "Item 2", ColSpan = 2 },
+                    new OpenHabThing() { Title = "Item 3" },
+                    new OpenHabThing() { Title = "Item 4", ColSpan = 2, RowSpan = 2 },
+                    new OpenHabThing() { Title = "Item 5" },
+                    new OpenHabThing() { Title = "Item 6", ColSpan = 2 }
+                }
+            });
+
             this.DataContext = this;
         }
     }
